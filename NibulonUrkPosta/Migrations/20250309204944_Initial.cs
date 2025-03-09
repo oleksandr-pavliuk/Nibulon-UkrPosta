ab2d4034c24dda296a00460833bb60c6f9d8382f
oleksandr-pavliuk <olexanderpavluk@gmail.com>
2025-03-09 22:52:25 +0200

    Added Migration

---
+++ NibulonUrkPosta/Migrations/20250309204944_Initial.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NibulonUrkPosta.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OBL",
+                columns: table => new
+                {
+                    OBL = table.Column<short>(type: "smallint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NOBL = table.Column<string>(type: "nvarchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OBL", x => x.OBL);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RAJ",
+                columns: table => new
+                {
+                    KRAJ = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RAJ = table.Column<string>(type: "nvarchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RAJ", x => x.KRAJ);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CITY",
+                columns: table => new
+                {
+                    CITY_KOD = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CITY = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    OBL = table.Column<short>(type: "smallint", nullable: false),
+                    KRAJ = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CITY", x => x.CITY_KOD);
+                    table.ForeignKey(
+                        name: "FK_CITY_OBL_OBL",
+                        column: x => x.OBL,
+                        principalTable: "OBL",
+                        principalColumn: "OBL",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_CITY_RAJ_KRAJ",
+                        column: x => x.KRAJ,
+                        principalTable: "RAJ",
+                        principalColumn: "KRAJ",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AUP",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    INDEX_A = table.Column<string>(type: "nvarchar(6)", nullable: false),
+                    CITY = table.Column<int>(type: "int", nullable: false),
+                    NCITY = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    OBL = table.Column<short>(type: "smallint", nullable: false),
+                    NOBL = table.Column<string>(type: "nvarchar(200)", nullable: false),
+                    RAJ = table.Column<int>(type: "int", nullable: false),
+                    NRAJ = table.Column<string>(type: "nvarchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AUP", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_AUP_CITY_CITY",
+                        column: x => x.CITY,
+                        principalTable: "CITY",
+                        principalColumn: "CITY_KOD",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AUP_OBL_OBL",
+                        column: x => x.OBL,
+                        principalTable: "OBL",
+                        principalColumn: "OBL",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AUP_RAJ_RAJ",
+                        column: x => x.RAJ,
+                        principalTable: "RAJ",
+                        principalColumn: "KRAJ",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AUP_CITY",
+                table: "AUP",
+                column: "CITY");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AUP_OBL",
+                table: "AUP",
+                column: "OBL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AUP_RAJ",
+                table: "AUP",
+                column: "RAJ");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CITY_KRAJ",
+                table: "CITY",
+                column: "KRAJ");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CITY_OBL",
+                table: "CITY",
+                column: "OBL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AUP");
+
+            migrationBuilder.DropTable(
+                name: "CITY");
+
+            migrationBuilder.DropTable(
+                name: "OBL");
+
+            migrationBuilder.DropTable(
+                name: "RAJ");
+        }
+    }
+}
--- NibulonUrkPosta/Persistance/ApplicationDbContext.cs
+++ NibulonUrkPosta/Persistance/ApplicationDbContext.cs
@@ -13,53 +13,56 @@ public class ApplicationDbContext : DbContext
     }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    base.OnModelCreating(modelBuilder);
+
+    modelBuilder.Entity<PostIndex>(entity =>
     {
-        base.OnModelCreating(modelBuilder);
+        entity.ToTable("AUP");
+        entity.HasKey(e => e.Id);
+        entity.Property(e => e.Id).HasColumnName("ID").ValueGeneratedOnAdd();
+        entity.Property(e => e.PostIndexCode).HasColumnName("INDEX_A").HasColumnType("nvarchar(6)");
+        entity.Property(e => e.CityId).HasColumnName("CITY").HasColumnType("int");
+        entity.Property(e => e.CityName).HasColumnName("NCITY").HasColumnType("nvarchar(200)");
+        entity.Property(e => e.DistrictId).HasColumnName("RAJ").HasColumnType("int");
+        entity.Property(e => e.DistrictName).HasColumnName("NRAJ").HasColumnType("nvarchar(200)");
+        entity.Property(e => e.RegionId).HasColumnName("OBL").HasColumnType("smallint");
+        entity.Property(e => e.RegionName).HasColumnName("NOBL").HasColumnType("nvarchar(200)");
 
-        modelBuilder.Entity<PostIndex>(entity =>
-        {
-            entity.ToTable("AUP");
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Id).HasColumnName("ID");
-            entity.Property(e => e.PostIndexCode).HasColumnName("INDEX_A");
-            entity.Property(e => e.CityId).HasColumnName("CITY");
-            entity.Property(e => e.CityName).HasColumnName("NCITY");
-            entity.Property(e => e.DistrictId).HasColumnName("RAJ");
-            entity.Property(e => e.DistrictName).HasColumnName("NRAJ");
-            entity.Property(e => e.RegionId).HasColumnName("OBL");
-            entity.Property(e => e.RegionName).HasColumnName("NOBL");
+        // Update foreign key relationships to prevent multiple cascade paths
+        entity.HasOne(e => e.City).WithMany().HasForeignKey(e => e.CityId).OnDelete(DeleteBehavior.Restrict);
+        entity.HasOne(e => e.District).WithMany().HasForeignKey(e => e.DistrictId).OnDelete(DeleteBehavior.Restrict);
+        entity.HasOne(e => e.Region).WithMany().HasForeignKey(e => e.RegionId).OnDelete(DeleteBehavior.Restrict);
+    });
 
-            entity.HasOne(e => e.City);
-            entity.HasOne(e => e.District);
-            entity.HasOne(e => e.Region);
-        });
+    modelBuilder.Entity<City>(entity =>
+    {
+        entity.ToTable("CITY");
+        entity.HasKey(e => e.Id);
+        entity.Property(e => e.Id).HasColumnName("CITY_KOD").ValueGeneratedOnAdd();
+        entity.Property(e => e.CityName).HasColumnName("CITY").HasColumnType("nvarchar(200)");
+        entity.Property(e => e.DistrictId).HasColumnName("KRAJ").HasColumnType("int");
+        entity.Property(e => e.RegionId).HasColumnName("OBL").HasColumnType("smallint");
 
-        modelBuilder.Entity<City>(entity =>
-        {
-            entity.ToTable("CITY");
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Id).HasColumnName("CITY_KOD");
-            entity.Property(e => e.CityName).HasColumnName("CITY");
-            entity.Property(e => e.DistrictId).HasColumnName("KRAJ");
-            entity.Property(e => e.RegionId).HasColumnName("OBL");
-            entity.HasOne(e => e.District).WithMany(e => e.Cities).HasForeignKey(d => d.DistrictId);
-            entity.HasOne(e => e.Region).WithMany(e => e.Cities).HasForeignKey(d => d.RegionId);
-        });
-        
-        modelBuilder.Entity<District>(entity =>
-        {
-            entity.ToTable("RAJ");
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Id).HasColumnName("KRAJ");
-            entity.Property(e => e.DistrictName).HasColumnName("RAJ");
-        });
-        
-        modelBuilder.Entity<Region>(entity =>
-        {
-            entity.ToTable("OBL");
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Id).HasColumnName("OBL");
-            entity.Property(e => e.RegionName).HasColumnName("NOBL");
-        });
-    }
+        // Update foreign key relationships to prevent multiple cascade paths
+        entity.HasOne(e => e.District).WithMany(e => e.Cities).HasForeignKey(d => d.DistrictId).OnDelete(DeleteBehavior.Restrict);
+        entity.HasOne(e => e.Region).WithMany(e => e.Cities).HasForeignKey(d => d.RegionId).OnDelete(DeleteBehavior.Restrict);
+    });
+
+    modelBuilder.Entity<District>(entity =>
+    {
+        entity.ToTable("RAJ");
+        entity.HasKey(e => e.Id);
+        entity.Property(e => e.Id).HasColumnName("KRAJ").HasColumnType("int");
+        entity.Property(e => e.DistrictName).HasColumnName("RAJ").HasColumnType("nvarchar(200)");
+    });
+
+    modelBuilder.Entity<Region>(entity =>
+    {
+        entity.ToTable("OBL");
+        entity.HasKey(e => e.Id);
+        entity.Property(e => e.Id).HasColumnName("OBL").HasColumnType("smallint");
+        entity.Property(e => e.RegionName).HasColumnName("NOBL").HasColumnType("nvarchar(200)");
+    });
+}
 }
